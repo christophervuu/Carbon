e55ff2bd9cb68c37ccc59898a17c109c21e49565
chris-laptop <christophervuu@live.com>
2020-03-12 11:41:35 -0500

    third commit

---
+++ app/src/main/java/com/example/carbon/Activities/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.carbon.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(SplashActivity.this, AuthenticationActivity.class));
+        finish();
+    }
+}
