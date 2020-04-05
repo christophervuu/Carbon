9e27861776bfdfa284d7c7e99ca8d633622a9e17
Christopher Vuu <christophervuu@live.com>
2020-04-05 18:57:56 -0500

    login

--- app/src/main/java/com/example/carbon/Activities/AuthenticationActivity.java
+++ app/src/main/java/com/example/carbon/Activities/AuthenticationActivity.java
@@ -6,24 +6,32 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.carbon.Activities.Fragments.DashboardFragment;
 import com.example.carbon.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class AuthenticationActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "EmailPassword";
+    private static final int RC_SIGN_IN = 9001;
 
     private FirebaseAuth mAuth;
     private GoogleSignInClient mGoogleSignInClient;
@@ -45,6 +53,7 @@ public class AuthenticationActivity extends AppCompatActivity implements View.On
         findViewById(R.id.SignInButton).setOnClickListener(this);
         findViewById(R.id.SignUpTextView).setOnClickListener(this);
         findViewById(R.id.ForgotPasswordTextView).setOnClickListener(this);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -102,6 +111,45 @@ public class AuthenticationActivity extends AppCompatActivity implements View.On
         // [END sign_in_with_email]
     }
 
+    private void FirebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+
+                        }
+                    }
+                });
+    }
+
+    private void GoogleSignIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                FirebaseAuthWithGoogle(account);
+
+            } catch (ApiException e) {
+
+            }
+        }
+
+    }
+
     private boolean validateForm() {
         boolean valid = true;
 
@@ -137,6 +185,8 @@ public class AuthenticationActivity extends AppCompatActivity implements View.On
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         } else if (i == R.id.SignInButton) {
             signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.sign_in_button) {
+            GoogleSignIn();
         }
     }
 }
